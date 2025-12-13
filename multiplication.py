2ffce94e35524d4974a579673ac33225f699d349
Sathish Govind <sathgovi@gmail.com>
2025-12-13 11:13:26 +0530

    create file multiplication

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+# Get input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Multiply the numbers
+product = num1 * num2
+
+# Display the result
+print(f"The product of {num1} and {num2} is {product}")
