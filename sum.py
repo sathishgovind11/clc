b587362c17858d7857ae6bc157352bd9894b386c
Sathish Govind <sathgovi@gmail.com>
2025-12-13 10:50:40 +0530

    create sum of 2 numbers using python

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Get two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
